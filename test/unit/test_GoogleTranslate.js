e4fdf26311830ea4267cac6ea11d749358790239
Éric Lemoine <eric.lemoine@gmail.com>
2010-08-11 06:57:15 +0200

    add tests for setLangPair and getLangPair, and complete the getGoogleUrl tests

--- test/unit/test_GoogleTranslate.js
+++ test/unit/test_GoogleTranslate.js
@@ -23,10 +23,45 @@ function test_prefs() {
    do_check_true(GoogleTranslate.prefs.getBoolPref("detectpagelang"));
 }
 
+function test_setLangPair() {
+    let from = GoogleTranslate.prefs.getCharPref("from");
+    let to = GoogleTranslate.prefs.getCharPref("to");
+
+    GoogleTranslate.setLangPair("fr", "en");
+
+    do_check_eq(GoogleTranslate.prefs.getCharPref("from"), "fr");
+    do_check_eq(GoogleTranslate.prefs.getCharPref("to"), "en");
+
+    GoogleTranslate.prefs.setCharPref("from", from);
+    GoogleTranslate.prefs.setCharPref("to", to);
+}
+
+function test_getLangPair() {
+    let from = GoogleTranslate.prefs.getCharPref("from");
+    let to = GoogleTranslate.prefs.getCharPref("to");
+
+    GoogleTranslate.prefs.setCharPref("from", "fr");
+    GoogleTranslate.prefs.setCharPref("to", "en");
+
+    let pair = GoogleTranslate.getLangPair();
+
+    do_check_eq(pair[0], "fr");
+    do_check_eq(pair[1], "en");
+
+    GoogleTranslate.prefs.setCharPref("from", from);
+    GoogleTranslate.prefs.setCharPref("to", to);
+}
+
 function test_getGoogleUrl() {
     let langFrom = "en", langTo = "fr", text = "project", url;
     url = GoogleTranslate.getGoogleUrl("api", langFrom, langTo, text);
     do_check_eq(url,
                 "http://ajax.googleapis.com/ajax/services/language/translate" +
                 "?v=1.0&format=text&langpair=en%7Cfr&q=project");
+    url = GoogleTranslate.getGoogleUrl("page", langFrom, langTo, text);
+    do_check_eq(url,
+                "http://translate.google.com/#en%7Cfr%7Cproject");
+    url = GoogleTranslate.getGoogleUrl("dict", langFrom, langTo, text);
+    do_check_eq(url,
+                "http://www.google.com/dictionary?langpair=en%7Cfr&q=project");
 }
