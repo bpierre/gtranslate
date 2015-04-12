3b7607cc5fef62d6b8fa8d04b81e5430c69c238b
Pierre Bertet <bonjour@pierrebertet.net>
2015-04-13 00:47:54 +0100

    Language selection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const cm = require('sdk/context-menu')
 const sp = require('sdk/simple-prefs')
+const ps = require('sdk/preferences/service')
 const { onMenuPopupshowing } = require('./menu-popupshowing')
 const { setTimeout } = require('sdk/timers')
 const { translate } = require('./providers/google-translate')
@@ -17,28 +18,95 @@ sp.on('checkall', () => {
 })
 
 // Context Menu
-const FROM = 'auto'
-const TO = 'fr'
 const LABEL_LOADING = 'Loading…'
 const LABEL_TRANSLATE = 'Translate “{0}”'
+const LABEL_CHANGE_FROM = 'Translate from {0}'
+const LABEL_CHANGE_TO = 'Translate to {0}'
 
 let lastSelectedText = ''
+
+function currentFrom() {
+  const prefFrom = sp.prefs.lang_from
+  return prefFrom
+}
+
+function currentTo() {
+  let prefTo = sp.prefs.lang_to
+  if (prefTo === 'auto') {
+    prefTo = ps.get('general.useragent.locale', 'en')
+    if (!prefTo.startsWith('zh')) {
+      prefTo = prefTo.replace(/-[a-zA-Z]+$/, '')
+    }
+  }
+  return prefTo
+}
 
 const translationItem = cm.Item({
   label: 'Fetching translation…',
   data: 'translation-item',
 })
 
-const languagesMenu = cm.Menu({
-  label: `Change languages (${FROM} → ${TO})`,
-  items: [  ]
+const langFromItems = languages => (
+  Object.keys(languages)
+    .filter(lang => !languages[lang].onlyTo)
+    .map(lang => cm.Item({
+      label: languages[lang].name,
+      data: lang,
+      contentScript: 'self.on("click", (node, data) => self.postMessage(data))',
+      onMessage: lang => updateFrom(lang),
+    }))
+)
+
+const langToItems = languages => (
+  Object.keys(languages)
+    .map(lang => cm.Item({
+      label: languages[lang].name,
+      data: lang,
+      contentScript: 'self.on("click", (node, data) => self.postMessage(data))',
+      onMessage: lang => updateTo(lang),
+    }))
+)
+
+const menuFrom = cm.Menu({
+  label: LABEL_CHANGE_FROM.replace(/\{0\}/, currentFrom()),
+  items: langFromItems(languages),
 })
+
+const menuTo = cm.Menu({
+  label: LABEL_CHANGE_TO.replace(/\{0\}/, currentTo()),
+  items: langToItems(languages),
+})
+
+const updateFrom = lang => {
+  sp.prefs.lang_from = lang
+  menuFrom.label = LABEL_CHANGE_FROM.replace(/\{0\}/, currentFrom())
+}
+
+const updateTo = lang => {
+  sp.prefs.lang_to = lang
+  menuTo.label = LABEL_CHANGE_TO.replace(/\{0\}/, currentTo())
+}
 
 const menu = cm.Menu({
   label: 'Translate',
   image: 'resource://gtranslate/assets/menuitem.svg',
   data: 'gtranslate-menu',
-  items: [ translationItem, cm.Separator(), languagesMenu ],
+  items: [
+    translationItem,
+    cm.Separator(),
+    menuFrom,
+    menuTo,
+    cm.Item({
+      label: 'Invert',
+      contentScript: 'self.on("click", () => self.postMessage())',
+      onMessage: () => {
+        const from = sp.prefs.lang_from
+        const to = sp.prefs.lang_to
+        updateTo(from)
+        updateFrom(to)
+      }
+    }),
+  ],
   contentScriptFile: 'resource://gtranslate/menu-contentscript.js',
   onMessage(text) {
     lastSelectedText = text.trim()
@@ -46,22 +114,22 @@ const menu = cm.Menu({
   }
 })
 
-function loading(itemNode) {
+const loading = itemNode => {
   itemNode.disabled = true
   itemNode.setAttribute('tooltiptext', '')
   translationItem.label = LABEL_LOADING
 }
 
-function loaded(itemNode, result) {
+const loaded = (itemNode, result) => {
   itemNode.disabled = false
   itemNode.setAttribute('tooltiptext', result)
   translationItem.label = result
 }
 
-onMenuPopupshowing(menu, function(menuNode) {
+onMenuPopupshowing(menu, menuNode => {
   const itemNode = menuNode.querySelector('[value="translation-item"]')
   loading(itemNode)
-  translate(FROM, TO, lastSelectedText, res => {
+  translate(currentFrom(), currentTo(), lastSelectedText, res => {
     loaded(itemNode, res.translation)
   })
 })
