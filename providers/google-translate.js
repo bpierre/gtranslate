7a2f7f4db85f0cae11b6e3ab785551f33d1bd378
mote0230 <winflip+github@gmail.com>
2015-12-17 15:52:04 +0100

    New token for google

--- providers/google-translate.js
+++ providers/google-translate.js
@@ -73,11 +73,40 @@ function translationResult(str, onError) {
 }
 
 // Some sort of token google uses
-function generateToken() {
-  const a = Math.floor((new Date).getTime() / 36E5) ^ 123456
-  return a + '|' + Math.floor(
-    (Math.sqrt(5) - 1) / 2 * (a ^ 654321) % 1 * 1048576
-  )
+generateToken = function(a) {
+  var b;
+  if (null === null) {
+    var c = String.fromCharCode(84);
+    b = String.fromCharCode(75);
+    c = [c, c];
+    c[1] = b;
+    b = Number(window[c.join(b)]) || 0
+  }
+  var d = String.fromCharCode(116),
+    c = String.fromCharCode(107),
+    d = [d, d];
+  d[1] = c;
+  for (var c = "&" + d.join("") +
+      "=", d = [], e = 0, f = 0; f < a.length; f++) {
+    var g = a.charCodeAt(f);
+    128 > g ? d[e++] = g : (2048 > g ? d[e++] = g >> 6 | 192 : (55296 == (g & 64512) && f + 1 < a.length && 56320 == (a.charCodeAt(f + 1) & 64512) ? (g = 65536 + ((g & 1023) << 10) + (a.charCodeAt(++f) & 1023), d[e++] = g >> 18 | 240, d[e++] = g >> 12 & 63 | 128) : d[e++] = g >> 12 | 224, d[e++] = g >> 6 & 63 | 128), d[e++] = g & 63 | 128)
+  }
+  a = b || 0;
+  for (e = 0; e < d.length; e++) a += d[e], a = tokenhelper(a, "+-a^+6");
+  a = tokenhelper(a, "+-3^+b+-f");
+  0 > a && (a = (a & 2147483647) + 2147483648);
+  a %= 1E6;
+  return (a.toString() + "." + (a ^ b))
+}
+
+tokenhelper = function(a, b) {
+  for (var c = 0; c < b.length - 2; c += 3) {
+    var d = b.charAt(c + 2),
+      d = d >= "a" ? d.charCodeAt(0) - 87 : Number(d),
+      d = b.charAt(c + 1) == "+" ? a >>> d : a << d;
+    a = b.charAt(c) == "+" ? a + d & 4294967295 : a ^ d
+  }
+  return a
 }
 
 function apiUrl(from, to, text) {
